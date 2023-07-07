f9683ee6879802905a51e27a2fa2b766e9119169
Abigailth1 <100715785+Abigailth1@users.noreply.github.com>
2023-07-07 19:07:55 -0400

    Main_Testing

---
+++ testing/testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Abigail Thomas on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
